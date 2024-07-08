09ad25bb66da2aab361c3b79a411a326747e56e0
Abu Jawad <abujawadsitol@gmail.com>
2024-07-08 17:51:46 +0600

    added custom carousel

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const carouselSlider = document.querySelector(".carousel-slider");
 const slides = document.querySelectorAll(".review");
 
 let slideNumber = 1;
+// this variable is used because slider button does not work after one click
 
+// next button functionalities
 nextButton.addEventListener("click", () => {
   if (slideNumber < slides.length) {
     carouselSlider.style.transform = `translateX(-${slideNumber * 100}vw)`;
@@ -15,13 +17,17 @@ nextButton.addEventListener("click", () => {
   }
 });
 
+// previous button functionalities
 prevButton.addEventListener("click", () => {
-  if (slideNumber >= 1) {
-    carouselSlider.style.transform = `translateX(${slideNumber * 100}vw)`;
+  if (slideNumber > 1) {
+    carouselSlider.style.transform = `translateX(-${
+      (slideNumber - 2) * 100
+    }vw)`;
     slideNumber--;
+  } else {
+    carouselSlider.style.transform = `translateX(-${
+      (slides.length - 1) * 100
+    }vw)`;
+    slideNumber = slides.length;
   }
-  //  else {
-  //   slideNumber = slides.length;
-  //   carouselSlider.style.transform = `translateX(${slideNumber * 100}vw)`;
-  // }
 });
