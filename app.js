5045dd36f651a0a92733f5dbbc3d4778fdc89dec
Abu Jawad <abujawadsitol@gmail.com>
2024-07-12 22:29:57 +0600

    some changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+// ***Carousel slider functionalities***
 const nextButton = document.querySelector(".next-btn");
 const prevButton = document.querySelector(".prev-btn");
 const carouselSlider = document.querySelector(".carousel-slider");
 const slides = document.querySelectorAll(".review");
 
-let slideNumber = 1;
-// this variable is used because slider button does not work after one click
+let slideNumber = 1; // this variable is used because slider button does not work after one click
 
 // next button functionalities
 nextButton.addEventListener("click", () => {
@@ -32,24 +32,30 @@ prevButton.addEventListener("click", () => {
   }
 });
 
-// product popup functionalities
+// ***Product popup modal functionalities***
 const productItems = document.querySelectorAll(".item");
 const productPopup = document.querySelector(".product-popup");
 const closePopupButton = document.querySelector(".close-popup-btn");
 const productSection = document.querySelector("#products");
 
-closePopupButton.addEventListener("click", () => {
-  productPopup.style.display = "none";
-});
-
+// open popup modal by clicking on a product card
 productItems.forEach((item, index) => {
   item.addEventListener("click", () => {
-    productPopup.style.display = "flex";
+    productPopup.style.transform = "translateY(0)";
+    productPopup.style.opacity = "1";
   });
 });
 
+// close popup modal by clicking on the x (top right)
+closePopupButton.addEventListener("click", () => {
+  productPopup.style.transform = "translateY(-300%)";
+  productPopup.style.opacity = "0";
+});
+
+// close popup modal by clicking outside the modal
 window.addEventListener("click", (event) => {
   if (event.target == productSection) {
-    productPopup.style.display = "none";
+    productPopup.style.transform = "translateY(-300%)";
+    productPopup.style.opacity = "0";
   }
 });
